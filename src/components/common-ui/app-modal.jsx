cbbed7d3fc38029dc638d8192b175036bfaf90ea
tinhvan <tinhvan@Tittle-Only.local>
2018-08-30 17:09:56 +0700

    full screen modal

--- src/components/common-ui/app-modal.jsx
+++ src/components/common-ui/app-modal.jsx
@@ -5,15 +5,13 @@ export default class AppModal extends Component{
 	render(){
 		const { id, height, label, children, full } = this.props
 		let style = {}
-		let modalStyle = {}
 		if(height) style = { height:height }
-		if(full) modalStyle = { minWidth: '100%', margin: '0px' }
 		return(
 			<div>
 				<button className="btn btn-primary btn-block" data-toggle="modal" data-target={"#"+id} style={style}>{label}</button>
 				<div className="modal fade" id={id} tabIndex="-1" role="dialog">
-				  <div className="modal-dialog" role="document" style={modalStyle}>
-				    <div className="modal-content">
+				  <div className="modal-dialog" role="document" style={{ minWidth: '100%', margin: '0px' }}>
+				    <div className="modal-content" style={{ minHeight:'100vh' }}>
 				    	<div className="modal-header">
 			                <button className="close" data-dismiss="modal">×</button>
 			            </div>
